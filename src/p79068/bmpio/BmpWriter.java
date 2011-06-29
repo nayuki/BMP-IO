cd64bb11b994dacac5d2db697f904d28e776b46a
Nayuki Minase <nayuki@eigenstate.org>
2011-06-29 16:26:18 +0000

    BmpWriter: Changed the write algorithm to buffer by row instead of the entire image.

--- src/p79068/bmpio/BmpWriter.java
+++ src/p79068/bmpio/BmpWriter.java
@@ -13,20 +13,11 @@ public final class BmpWriter {
 		int width = image.getWidth();
 		int height = image.getHeight();
 		int rowSize = (width * 3 + 3) / 4 * 4;  // 3 bytes per pixel in RGB888, round up to multiple of 4
-		byte[] imageBytes = new byte[rowSize * height];
-		
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				int color = image.getRgb888Pixel(x, y);
-				imageBytes[(height - 1 - y) * rowSize + x * 3 + 0] = (byte)(color >>>  0);  // Blue
-				imageBytes[(height - 1 - y) * rowSize + x * 3 + 1] = (byte)(color >>>  8);  // Green
-				imageBytes[(height - 1 - y) * rowSize + x * 3 + 2] = (byte)(color >>> 16);  // Red
-			}
-		}
+		int imageSize = rowSize * height;
 		
 		// BITMAPFILEHEADER
 		out1.writeBytes(new byte[]{'B', 'M'}, 0, 2);   // FileType
-		out1.writeInt32(14 + 40 + imageBytes.length);  // FileSize
+		out1.writeInt32(14 + 40 + imageSize);          // FileSize
 		out1.writeInt16(0);                            // Reserved1
 		out1.writeInt16(0);                            // Reserved2
 		out1.writeInt32(14 + 40);                      // BitmapOffset
@@ -38,13 +29,22 @@ public final class BmpWriter {
 		out1.writeInt16(1);                         // Planes
 		out1.writeInt16(24);                        // BitsPerPixel
 		out1.writeInt32(0);                         // Compression
-		out1.writeInt32(imageBytes.length);         // SizeOfBitmap
+		out1.writeInt32(imageSize);                 // SizeOfBitmap
 		out1.writeInt32(bmp.horizontalResolution);  // HorzResolution
 		out1.writeInt32(bmp.verticalResolution);    // VertResolution
 		out1.writeInt32(0);                         // ColorsUsed
 		out1.writeInt32(0);                         // ColorsImportant
 		
-		out1.writeBytes(imageBytes, 0, imageBytes.length);
+		byte[] row = new byte[rowSize];
+		for (int y = height - 1; y >= 0; y--) {
+			for (int x = 0; x < width; x++) {
+				int color = image.getRgb888Pixel(x, y);
+				row[x * 3 + 0] = (byte)(color >>>  0);  // Blue
+				row[x * 3 + 1] = (byte)(color >>>  8);  // Green
+				row[x * 3 + 2] = (byte)(color >>> 16);  // Red
+			}
+			out1.writeBytes(row, 0, row.length);
+		}
 		
 		out1.flush();
 	}
