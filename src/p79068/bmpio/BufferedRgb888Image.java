86a21b0a9c552b0def89e203aec1ae74a2e256b2
Nayuki Minase <nayuki@eigenstate.org>
2011-06-29 16:55:49 +0000

    BufferedRgb888Image: Added bounds checks.

--- src/p79068/bmpio/BufferedRgb888Image.java
+++ src/p79068/bmpio/BufferedRgb888Image.java
@@ -30,11 +30,15 @@ public final class BufferedRgb888Image implements Rgb888Image {
 	
 	
 	public int getRgb888Pixel(int x, int y) {
+		if (x < 0 || x >= width || y < 0 || y >= height)
+			throw new IndexOutOfBoundsException();
 		return pixels[y * width + x];
 	}
 	
 	
 	public void setRgb888Pixel(int x, int y, int color) {
+		if (x < 0 || x >= width || y < 0 || y >= height)
+			throw new IndexOutOfBoundsException();
 		pixels[y * width + x] = color;
 	}
 	
