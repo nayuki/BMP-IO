2bff7697b851ef9d60060990524a5eb6c6ae2802
Nayuki Minase <nayuki@eigenstate.org>
2011-07-27 21:22:22 +0000

    BmpReader: Added lots of checks for the correct field values, heavily edited code.

--- src/p79068/bmpio/BmpReader.java
+++ src/p79068/bmpio/BmpReader.java
@@ -8,41 +8,69 @@ import java.io.InputStream;
 public final class BmpReader {
 	
 	public static BmpImage read(InputStream in) throws IOException {
-		// BITMAPFILEHEADER
-		if (readInt16(in) != 0x4D42)
-			throw new RuntimeException();
-		skipFully(in, 12);
+		// BITMAPFILEHEADER (14 bytes)
+		if (readInt16(in) != 0x4D42)  // "BM"
+			throw new RuntimeException("Invalid BMP signature");
+		int fileSize = readInt32(in);
+		skipFully(in, 4);  // Skip reserved
+		int imageDataOffset = readInt32(in);
 		
-		// BITMAPINFOV3HEADER
-		if (readInt32(in) != 40)
-			throw new RuntimeException();
-		int width  = readInt32(in);
-		int height = readInt32(in);
-		if (readInt16(in) != 1)  // Planes
-			throw new RuntimeException();
-		int bpp = readInt16(in);  // BitsPerPixel
-		if (bpp != 24 && bpp != 32)
-			throw new RuntimeException();
-		if (readInt32(in) != 0)  // Compression
-			throw new RuntimeException();
-		
+		// BITMAPINFOHEADER
 		BmpImage bmp = new BmpImage();
+		int headerSize = readInt32(in);
+		int width;
+		int height;
+		int bitsPerPixel;
+		int imageSize;
+		if (headerSize == 40) {
+			width  = readInt32(in);
+			height = readInt32(in);
+			int planes = readInt16(in);
+			bitsPerPixel = readInt16(in);
+			int compression = readInt32(in);
+			imageSize = readInt32(in);
+			bmp.horizontalResolution = readInt32(in);
+			bmp.verticalResolution   = readInt32(in);
+			int colorsUsed = readInt32(in);
+			int colorsImportant = readInt32(in);
+			
+			if (width <= 0)
+				throw new RuntimeException("Invalid width: " + width);
+			if (height <= 0)
+				throw new RuntimeException("Invalid height: " + height);
+			if (planes != 1)
+				throw new RuntimeException("Unsupported planes: " + planes);
+			if (bitsPerPixel != 24 && bitsPerPixel != 32)
+				throw new RuntimeException("Unsupported bits per pixel: " + bitsPerPixel);
+			if (imageSize != (width * bitsPerPixel / 8 + 3) / 4 * 4 * height)
+				throw new RuntimeException("Invalid image size: " + imageSize);
+			if (compression != 0)
+				throw new RuntimeException("Unsupported compression: " + compression);
+			if (colorsUsed != 0)
+				throw new RuntimeException("Invalid colors used: " + colorsUsed);
+			if (colorsImportant != 0)
+				throw new RuntimeException("Invalid important colors: " + colorsImportant);
+			
+		} else
+			throw new RuntimeException("Unsupported BMP header format: " + headerSize + " bytes");
+		
+		if (14 + headerSize > imageDataOffset)
+			throw new RuntimeException("Invalid image data offset: " + imageDataOffset);
+		if (imageDataOffset + imageSize > fileSize)
+			throw new RuntimeException("Invalid file size: " + fileSize);
+		
 		BufferedRgb888Image image = new BufferedRgb888Image(width, height);
 		bmp.image = image;
-		skipFully(in, 4);
 		
-		bmp.horizontalResolution = readInt32(in);
-		bmp.verticalResolution   = readInt32(in);
-		skipFully(in, 8);
-		
-		bpp /= 8;  // Now bpp is bytes per pixel
-		byte[] row = new byte[(width * bpp + 3) / 4 * 4];
+		// Read the image data
+		int bytesPerPixel = bitsPerPixel / 8;
+		byte[] row = new byte[(width * bytesPerPixel + 3) / 4 * 4];
 		for (int y = height - 1; y >= 0; y--) {
 			readFully(in, row);
 			for (int x = 0; x < width; x++) {
-				int color =   (row[x * bpp + 2] & 0xFF) << 16
-				            | (row[x * bpp + 1] & 0xFF) <<  8
-				            | (row[x * bpp + 0] & 0xFF) <<  0;
+				int color =   (row[x * bytesPerPixel + 2] & 0xFF) << 16
+				            | (row[x * bytesPerPixel + 1] & 0xFF) <<  8
+				            | (row[x * bytesPerPixel + 0] & 0xFF) <<  0;
 				image.setRgb888Pixel(x, y, color);
 			}
 		}
